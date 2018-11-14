7a2b0ae9feef619c6419a64a52e5890184bed9e5
tpgraham <tpgraha@clemson.edu>
2018-11-14 09:47:25 -0500

    updated frontend

--- driver/app.js
+++ driver/app.js
@@ -12,7 +12,7 @@ xhttp.open("POST", "http://server.isaacmcqueen.me:9615/action", true);
 xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    let JSONval = JSON.parse(this.responseText);
+    JSONval = JSON.parse(this.responseText);
 
   }
 };
