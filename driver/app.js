e970031ceb4950fe76070e4f0b8a7f7a4cf19a48
tpgraham <tpgraha@clemson.edu>
2018-11-14 10:59:01 -0500

    updated frontend

--- driver/app.js
+++ driver/app.js
@@ -3,7 +3,7 @@
 // var orderIds = ["241067840","245232145"];
 
 /* Set the width of the side navigation to 250px */
-var JSONval="";
+var JSONval={};
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
