4e015ad27382eb5a5d78af3bae47f0e70370cbb2
Isaac McQueen <imcqueen@truehomesusa.com>
2025-08-02 18:36:11 -0400

    wip

--- flake.nix
+++ flake.nix
@@ -10,99 +10,39 @@
     in
     {
       devShells = forAllSystems ({ pkgs }:
-        let
-          layoutFile = pkgs.writeText "layout.kdl" ''
-            layout {
-              pane split_direction="horizontal" {
-                  pane {
-                    command "hx"
-                    args "."
-                  }
-                  pane size="20%"
-              }
-            }
-          '';
-        in
         {
           default = pkgs.mkShell {
-            packages = with pkgs;
-              [
-                gleam
-                erlang_27
-                rebar3
-                flyctl
-                tailwindcss
-                tailwindcss-language-server
-                mermaid-cli
-              ]
-              ++ (if pkgs.stdenv.isLinux then [ inotify-tools ] else [ ]);
+            packages = with pkgs; [
+              gleam
+              erlang_27
+              rebar3
+              act
+              colima
+            ]
+            ++
+            pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs; [
+              inotify-tools
+            ]);
             shellHook = ''
-              if [ -z "$ZELLIJ" ] || [ "$ZELLIJ" -ne 0 ]; then
-                zellij -l ${layoutFile}
-              fi
+              export DOCKER_HOST="unix:///Users/imcquee/.config/colima/default/docker.sock"
             '';
           };
         });
-
-      apps = forAllSystems ({ pkgs }: {
-        render = {
-          type = "app";
-          program = toString (pkgs.writeScript "render" ''
-            #!/bin/bash
-
-            # Clean
-            echo "Cleaning"
-            rm -rf ./priv
-
-            sleep 2
-
-            Run the Gleam build
-            echo "Running Gleam build..."
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.stdenv.mkDerivation {
+          pname = "website";
+          version = "0.1.0"; # Add this line
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ gleam erlang_27 rebar3 ];
+          buildPhase = ''
+            export HOME=$TMPDIR
+            export XDG_CACHE_HOME=$TMPDIR/gleam-cache
+            mkdir -p "$XDG_CACHE_HOME/gleam/hex/hexpm/packages"
             gleam run -m build
-
-            # Check if the Gleam build succeeded
-            if [ $? -eq 0 ]; then
-              echo "Gleam build completed successfully."
-            else
-              echo "Gleam build failed. Exiting."
-              exit 1
-            fi
-
-            sleep 2
-
-            echo "Building Tailwind CSS"
-
-            tailwindcss -i ./static/input.css -o ./static/custom.css --minify
-
-            # sleep 3
-
-            echo "Opening HTML"
-
-            # Get the full path to the file
-            FILE_PATH="$(pwd)/priv/index.html"
-
-            # Function to open the file in the default web browser
-            open_file() {
-              if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-                if grep -qi microsoft /proc/version; then
-                  # Running in WSL
-                  powershell.exe start "$(wslpath -w "$FILE_PATH")"
-                else
-                  # Native Linux
-                  xdg-open "$FILE_PATH" 2>/dev/null
-                fi
-              elif [[ "$OSTYPE" == "darwin"* ]]; then
-                open "$FILE_PATH"
-              elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-                start "" "$FILE_PATH"
-              else
-                echo "Unsupported OS"
-                exit 1
-              fi
-            }
-            # Run the function
-            open_file
-          '');
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+          '';
         };
       });
     };
